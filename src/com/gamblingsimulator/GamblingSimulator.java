7cda0ec9eb6c3370280a4c81bc3577fb68c0f070
atul080 <atulsrivastav16k26@gmaial.com>
2021-12-29 09:13:11 +0530

    Added: Gambler win-loose check program added.

--- src/com/gamblingsimulator/GamblingSimulator.java
+++ src/com/gamblingsimulator/GamblingSimulator.java
@@ -1,26 +1,40 @@
 package com.gamblingsimulator;
+
+import java.util.Random;
+
 /*
  * @author Atul Srivastav
  * @date 29/12/2021
  * To make a gambler simulator program which starts with a stake and bets every game to win.
+ * Gambler bets in every game to win or loose some stake. He exits if stake reaches maximum or minuimum limit.
  */
-
-import java.util.Scanner;
-
 public class GamblingSimulator {
-    /*
-     * Gambler bets in every game to win or loose some stake. He exits if stake reaches maximum or minuimum limit.
-     */
-
     //initializing variables
-    static int stake=100;
-    static int bet=1;
-    public static void main(String[] ar) {
+    public static int STAKE=100;
+    public static final int BET=1;
 
+    //BET METHOD
+    public static void checkBet(int betVal)
+    {
+        if(betVal==BET)
+        {
+            System.out.println("Gambler WON the bet.");
+        }
+        else
+        {
+            System.out.println("Gambler LOOSE the bet.");
+        }
+
+    }
+    public static void main(String[] ar) {
         //welcome message
         System.out.println("Welcome to gambling simulator. ");
 
-
-
+        //making object of Random Class and getting a random bet value between 0 and 1
+        Random rm=new Random();
+        int betVal=rm.nextInt(2);
+        
+        //calling checkBet method to check if gambler won the bet or not.
+        GamblingSimulator.checkBet(betVal);
     }
 }
