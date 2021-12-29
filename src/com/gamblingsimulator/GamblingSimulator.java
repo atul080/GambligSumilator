de5e2b9fbf044775773ce69c04f4cccdc1f9060b
atul080 <atulsrivastav16k26@gmaial.com>
2021-12-29 08:25:43 +0530

    Added: Welcome program added for gambler simulator.

---
+++ src/com/gamblingsimulator/GamblingSimulator.java
@@ -0,0 +1,19 @@
+package com.gamblingsimulator;
+/*
+ * @author Atul Srivastav
+ * @date 29/12/2021
+ * To make a gambler simulator program which starts with a stake and bets every game to win.
+ */
+
+import java.util.Scanner;
+
+public class GamblingSimulator {
+    public static void main(String[] ar) {
+        /*
+         * Gambler bets in every game to win or loose some stake. He exits if stake reaches maximum or minuimum limit.
+         */
+
+        //welcome message
+        System.out.println("Welcome to gambling simulator.");
+    }
+}
